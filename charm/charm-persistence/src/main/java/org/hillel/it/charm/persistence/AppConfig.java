14520cda736be0bf1325473fec82fdaa4870ae13
HrankinaNastya <klava120192@mail.ru>
2014-10-15 14:25:12 +0300

    Deleted some files

--- charm/charm-persistence/src/main/java/org/hillel/it/charm/persistence/AppConfig.java
+++ charm/charm-persistence/src/main/java/org/hillel/it/charm/persistence/AppConfig.java
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @PropertySource("classpath:application.properties")
 @EnableTransactionManagement
+@ComponentScan("org.hillel.it")
 public class AppConfig {
 		
 		@Autowired
